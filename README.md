a826254eb294ab786b03a4071cd49ca14d47ea62
Mat Moore <matmoore@users.noreply.github.com>
2023-04-01 13:01:02 +0100

    Add regression testing functionality to runner
    This allows me to change library code and verify I haven't broken any
    earlier solutions.
    
    This uses the "golden master" pattern: store a "known good"
    output and compare all future outputs to that one.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Rosalind problems
 
 Solutions to problems at https://rosalind.info/problems
+
+## To run the solutions
+
+Run `ruby runner.rb` to run all the solutions.
--- runner.rb
+++ runner.rb
@@ -22,9 +22,27 @@ last_index = solution_modules.size - 1
 solution_modules.each_with_index do |m, i|
   name = m.name.sub('Solutions::', '')
   lowercase = name.downcase
+
   test_input = File.read("inputs/#{lowercase}.txt").chomp
 
-  puts name
-  puts m.solution(test_input)
+  output_path = "outputs/#{lowercase}.txt"
+
+  begin
+    original_output = File.read(output_path)
+  rescue Errno::ENOENT
+    original_output = nil
+  end
+
+  output = m.solution(test_input).to_s + "\n"
+
+  if original_output.nil?
+    File.write(output_path, output)
+  elsif original_output != output
+    puts "#{name} FAILED"
+  else
+    puts name
+  end
+
+  puts output
   puts "" if i != last_index
 end
